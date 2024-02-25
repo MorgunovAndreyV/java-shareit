323c5aff0b68bf5007babf979749ae17cb702e7d
Administrator <123@123.ru>
2024-02-25 13:54:22 +0300

    fix. Удалил лишнее #2

--- src/main/java/ru/practicum/shareit/comparators/BookingComparators.java
+++ src/main/java/ru/practicum/shareit/comparators/BookingComparators.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.comparators;
 
 
-import ru.practicum.shareit.booking.Booking;
 import ru.practicum.shareit.booking.dto.BookingDto;
 
 import java.util.Comparator;
--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item.dto;
 import lombok.Builder;
 import lombok.Data;
 import ru.practicum.shareit.booking.dto.BookingDto;
-import ru.practicum.shareit.item.model.Comment;
 
 import java.util.List;
 
